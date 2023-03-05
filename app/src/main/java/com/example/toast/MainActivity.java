3f92578e1f8bd7c4ecd8d7f3e4588dad5e3233c8
netosk81 <hk.neto@gmail.com>
2023-03-05 10:21:43 -0300

    Criando comando Toast customizado para mostrar algo diferente do padrão que é a mensagem, neste caso estou mostrando uma imagem de estrela, mas poderia ser qualquer outra imagem ou mensagem

--- app/src/main/java/com/example/toast/MainActivity.java
+++ app/src/main/java/com/example/toast/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,7 +17,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void abrirToast(View view){
 
-        Toast.makeText(this, "Ação Realizada com sucesso", Toast.LENGTH_SHORT).show();
+        ImageView imagem = new ImageView(getApplicationContext());
+        imagem.setImageResource(android.R.drawable.star_big_off);
+
+        Toast toast = new Toast( getApplicationContext());
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(imagem);
+        toast.show();
+
+        //Toast.makeText(this, "Ação Realizada com sucesso", Toast.LENGTH_SHORT).show();
 
     }
 
