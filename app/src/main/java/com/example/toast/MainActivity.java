60b2ca870b0d0c93c58e885259fc64719513bea8
netosk81 <hk.neto@gmail.com>
2023-03-05 10:10:08 -0300

    Initial commit

---
+++ app/src/main/java/com/example/toast/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void abrirToast(View view){
+
+        Toast.makeText(this, "Ação Realizada com sucesso", Toast.LENGTH_SHORT).show();
+
+    }
+
+}
